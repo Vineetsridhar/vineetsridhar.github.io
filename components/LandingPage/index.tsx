e7ea77669cdde2d00a75673b8458ff66a2664cb9
Vineet Sridhar <vineets1600@gmail.com>
2022-10-10 23:33:06 -0400

    Changed typing animation package to support emojis

--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -1,27 +1,31 @@
 import Image from "next/image";
-import ReactTyped from "react-typed";
+import { TypeAnimation } from "react-type-animation";
 import { LandingContainer } from "./styled";
+const DESCRIPTIONS = [
+    "Software engineer 🧑💻",
+    "Lifelong learner 🧑🎓",
+    "Hiker ⛰️",
+    "Leader 👑",
+    "Nature lover 🌻"
+];
+
+const generateTypingSequence = (backspaceDelay: number) => {
+    return DESCRIPTIONS.map((description) => [description, backspaceDelay]).flat()
+}
 
 const LandingPage = () => {
-    const c = "dfd";
     return (
         <LandingContainer>
             <div className="title-container">
                 <h1>Vineet Sridhar</h1>
-                <ReactTyped
-                    strings={[
-                        "Software engineer",
-                        "Lifelong learner",
-                        "Hiker",
-                        "Leader",
-                        "Nature lover"
-                    ]}
-                    typeSpeed={50}
-                    backSpeed={50}
-                    backDelay={3000}
-                    loop
+                <TypeAnimation
+                    sequence={generateTypingSequence(3000)}
+                    wrapper="div"
+                    cursor
+                    repeat={Infinity}
                     className="description"
                 />
+
 
             </div>
             <img
