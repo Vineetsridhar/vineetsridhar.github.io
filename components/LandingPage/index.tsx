4e98c1f0a3da6b8b9dd1eb9b906fbe1cef31b2e9
Vineet Sridhar <vineets1600@gmail.com>
2022-10-10 23:33:06 -0400

    Changed typing animation package to support emojis

--- components/LandingPage/index.tsx
+++ components/LandingPage/index.tsx
@@ -2,38 +2,35 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { LandingContainer } from "./styled";
 const DESCRIPTIONS = [
-    "Software engineer 🧑💻",
-    "Lifelong learner 🧑🎓",
-    "Hiker ⛰️",
-    "Leader 👑",
-    "Nature lover 🌻"
+  "Software engineer 🧑💻",
+  "Lifelong learner 🧑🎓",
+  "Hiker ⛰️",
+  "Leader 👑",
+  "Nature lover 🌻",
 ];
 
 const generateTypingSequence = (backspaceDelay: number) => {
-    return DESCRIPTIONS.map((description) => [description, backspaceDelay]).flat()
-}
+  return DESCRIPTIONS.map((description) => [
+    description,
+    backspaceDelay,
+  ]).flat();
+};
 
 const LandingPage = () => {
-    return (
-        <LandingContainer>
-            <div className="title-container">
-                <h1>Vineet Sridhar</h1>
-                <TypeAnimation
-                    sequence={generateTypingSequence(3000)}
-                    wrapper="div"
-                    cursor
-                    repeat={Infinity}
-                    className="description"
-                />
-
-
-            </div>
-            <img
-                alt="Picture of me"
-                src="/profile.jpg"
-                className="image"
-            />
-        </LandingContainer>
-    )
-}
+  return (
+    <LandingContainer>
+      <div className="title-container">
+        <h1>Vineet Sridhar</h1>
+        <TypeAnimation
+          sequence={generateTypingSequence(3000)}
+          wrapper="div"
+          cursor
+          repeat={Infinity}
+          className="description"
+        />
+      </div>
+      <img alt="Picture of me" src="/profile.jpg" className="image" />
+    </LandingContainer>
+  );
+};
 export default LandingPage;
