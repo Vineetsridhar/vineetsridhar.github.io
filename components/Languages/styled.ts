f1bbaba1b8cf2b04ee4d80dfa351199174345ed4
vineetsridhar <vineets1600@gmail.com>
2022-10-16 17:32:50 -0400

    Fixed CSS styling issue

--- components/Languages/styled.ts
+++ components/Languages/styled.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const LangaugesContainer = styled.section`
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   align-items: center;
   padding-top: 16px;
